73dd53d07b6e74d36c99753e63454cb103ce5ac0
barbara-pietoso <153303973+barbara-pietoso@users.noreply.github.com>
2024-12-11 13:10:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,34 @@
+kml_carregado = False
 
+arquivo_bacias = "/content/Bacia_Hidrografica.shp"
+
+dic_cod_bacia = {
+    'gravataí': "dados/mini_bacias/G010_mini_19_02.shp",
+    'sinos': "dados/mini_bacias/G020_mini_19_02.shp",
+    'caí': "dados/mini_bacias/G030_mini_19_02.shp",
+    'taquari-antas': "dados/mini_bacias/G040_mini_19_02.shp",
+    'alto jacuí': "dados/mini_bacias/G050_mini_19_02.shp",
+    'vacacaí-vacacaí-mirim': "dados/mini_bacias/G060_mini_19_02.shp",
+    'baixo jacuí': "dados/mini_bacias/G070_mini_19_02.shp",
+    'lago guaíba': "dados/mini_bacias/G080_mini_19_02.shp",
+    'pardo': "dados/mini_bacias/G090_mini_19_02.shp",
+    'tramandaí': "dados/mini_bacias/L010_mini_19_02.shp",
+    'litoral médio': "dados/mini_bacias/L020_mini_19_02.shp",
+    'camaquã': "dados/mini_bacias/L030_mini_19_02.shp",
+    'mirim são gonçalo': "dados/mini_bacias/L040_mini_19_02.shp",
+    'mampituba': "dados/mini_bacias/L050_mini_19_02.shp",
+    'apuaê-inhandava': "dados/mini_bacias/U010_mini_19_02.shp",
+    'passo fundo': "dados/mini_bacias/U020_min_19_02.shp",
+    'turvo santa rosa santo cristo': "dados/mini_bacias/U030_mini_19_02.shp",
+    'piratinim': "dados/mini_bacias/U040_mini_19_02.shp",
+    'ibicuí': "dados/mini_bacias/U050_mini_19_02.shp",
+    'quaraí': "dados/mini_bacias/U060_mini_19_02.shp",
+    'santa maria': "dados/mini_bacias/U070_mini_19_02.shp",
+    'negro': "dados/mini_bacias/U080_mini_19_02.shp",
+    'ijuí': "dados/mini_bacias/U090_mini_19_02.shp",
+    'várzea': "dados/mini_bacias/U100_mini_19_02.shp",
+    'butuí-icamaquã': "dados/mini_bacias/U110_mini_19_02.shp",
+}
+
+#print('Essa janela de terminal é aberta para mostrar a execução do software e possíveis erros.')
+#print('Envie qualquer mensagem de erro encontrada para roberto-rolo@sema.rs.gov.br.\n')
