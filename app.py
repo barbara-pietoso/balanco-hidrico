457bdb10c7bd05d99ce3607b2e6445f53a362efe
barbara-pietoso <153303973+barbara-pietoso@users.noreply.github.com>
2025-01-15 11:35:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -29,8 +29,9 @@ col2.subheader("Insira as Coordenadas:")
 latitude = col2.number_input("Latitude", min_value=-90.0, max_value=90.0, step=0.0001, format="%.4f")
 longitude = col2.number_input("Longitude", min_value=-180.0, max_value=180.0, step=0.0001, format="%.4f")
 
-# URL do arquivo GeoJSON hospedado no Google Drive (link direto)
-file_url = "https://drive.google.com/uc?export=download&id=1--laLHmabXElImR5RT-j-c4FajYICPqJ"
+# Coordenadas iniciais do centro do Rio Grande do Sul
+default_latitude = -30.0
+default_longitude = -53.5
 
 # Exibir as coordenadas inseridas
 if latitude and longitude:
@@ -39,6 +40,9 @@ if latitude and longitude:
     # Criar mapa centrado nas coordenadas inseridas
     mapa = folium.Map(location=[latitude, longitude], zoom_start=12)
 
+    # Carregar o arquivo GeoJSON diretamente da URL
+    file_url = "https://drive.google.com/uc?export=download&id=1--laLHmabXElImR5RT-j-c4FajYICPqJ"
+    
     try:
         # Carregar o arquivo GeoJSON diretamente da URL
         response = requests.get(file_url)
@@ -50,6 +54,28 @@ if latitude and longitude:
 
         # Adicionar um marcador no mapa
         folium.Marker([latitude, longitude], popup="Coordenadas Inseridas").add_to(mapa)
+
+        # Exibir o mapa no Streamlit
+        st_folium(mapa, width=700, height=500)
+
+    except requests.exceptions.RequestException as e:
+        col2.write(f"Erro ao carregar o arquivo GeoJSON: {e}")
+
+else:
+    # Criar mapa centralizado no centro do Rio Grande do Sul (sem coordenadas inseridas)
+    mapa = folium.Map(location=[default_latitude, default_longitude], zoom_start=7)
+
+    # Carregar o arquivo GeoJSON diretamente da URL
+    file_url = "https://drive.google.com/uc?export=download&id=1--laLHmabXElImR5RT-j-c4FajYICPqJ"
+    
+    try:
+        # Carregar o arquivo GeoJSON diretamente da URL
+        response = requests.get(file_url)
+        response.raise_for_status()  # Verifica se houve algum erro na requisição
+        geojson_data = response.json()
+
+        # Adicionar o arquivo GeoJSON ao mapa
+        folium.GeoJson(geojson_data).add_to(mapa)
 
         # Exibir o mapa no Streamlit
         st_folium(mapa, width=700, height=500)
