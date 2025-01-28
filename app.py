e03a1e267a24c5c95ffb8d145ed3437c09ff1504
barbara-pietoso <153303973+barbara-pietoso@users.noreply.github.com>
2025-01-28 12:54:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ col1, col2, col3 = st.columns([1,2,1])
 
 col3.image('https://github.com/barbara-pietoso/disponibilidade-hidrica-rs/blob/main/Bras%C3%A3o---RS---Sema%20(2).png?raw=true', width=300)
 col2.title('        Disponibilidade Hídrica para Outorga')
-col1.image('https://github.com/barbara-pietoso/disponibilidade-hidrica-rs/blob/main/drhslogo.png?raw=true', width=150)
+col1.image('https://github.com/barbara-pietoso/disponibilidade-hidrica-rs/blob/main/drhslogo.png?raw=true', width=200)
 
 
 # Limites aproximados de latitude e longitude do Rio Grande do Sul
